1fa07c0adbf12e175ac913736cf42c77d8cbc924
joaoassisnabais <joaomarcos.nabais@gmail.com>
2022-12-16 12:19:49 +0000

    fix bots

--- src/bots/bots.c
+++ src/bots/bots.c
@@ -7,23 +7,29 @@
 #include <unistd.h>
 #include <string.h>
 
-#include "bots.h"
 #include "../common/defs.h"
 #include "../common/message.h"
+#include "../common/connection.h"
 
-int main(int argc, char **argv ){
-    char bot_addr[108];
-    struct sockaddr_un serv_addr;
+int num_bots;
+int dgram_socket;
 
-    sscanf(argv[2], "%d", &num_bots);
+void send_msg(char *msg_txt, struct sockaddr_un servaddr){
+    char command[16];
+    message msg;
+    sscanf(msg_txt, "%s", command);
+    msg.state.num_bots = num_bots;
+    create_message(&msg, msg_txt, NULL);
 
-    sprintf(bot_addr, "/tmp/client%d", getpid());
-    dgram_socket = unix_socket_init(bot_addr);
-    serv_addr = getAddr(argv[1]);
+    if(strcmp(command, "bot_connect") == 0){
+        sendto(dgram_socket, &msg, sizeof(msg), 0, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
-    bot_loop(serv_addr);
-    
-    exit(0);
+    }else if(strcmp(command, "bot_move") == 0){
+        sendto(dgram_socket, &msg, sizeof(msg), 0, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    }else if(strcmp(command, "bot_disconnect") == 0){
+        sendto(dgram_socket, &msg, sizeof(msg), 0, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    }
 }
 
 //create bot that opens a udp socket and sends a connect message to the server and then implements a while that allows the user to move the player and sends the new position to the server unless the user presses q to quit or esc
@@ -48,21 +54,15 @@ void bot_loop(struct sockaddr_un serv_addr){
     close(dgram_socket);
 }
 
-void send_msg(char *msg_txt, struct sockaddr_un servaddr){
-    char command[16];
-    message msg;
-    sscanf(msg_txt, "%s", command);
-    msg.state.num_bots = num_bots;
-    strncpy(msg.txt, msg_txt, MSG_TXT_SIZE);
+int main(int argc, char **argv ){
+    char bot_addr[108];
 
+    sscanf(argv[2], "%d", &num_bots);
 
-    if(strcmp(command, "bot_connect") == 0){
-        sendto(dgram_socket, &msg, sizeof(msg), 0, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    sprintf(bot_addr, "/tmp/bot%d", getpid());
+    dgram_socket = unix_socket_init(bot_addr);
 
-    }else if(strcmp(command, "bot_move") == 0){
-        sendto(dgram_socket, &msg, sizeof(msg), 0, (struct sockaddr *) &servaddr, sizeof(servaddr));
-
-    }else if(strcmp(command, "bot_disconnect") == 0){
-        sendto(dgram_socket, &msg, sizeof(msg), 0, (struct sockaddr *) &servaddr, sizeof(servaddr));
-    }
+    bot_loop(get_addr(argv[1]));
+    
+    exit(0);
 }
