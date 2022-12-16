5db6a5e39d3863d2a2ab2feb74f05f78f8cf944a
Rui Pires <pires.rui@outlook.pt>
2022-12-16 00:12:01 +0000

    fix client

--- src/client/client.c
+++ src/client/client.c
@@ -1,66 +1,42 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <sys/un.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <ncurses.h>    
 #include <string.h>
+#include <stdlib.h>
 
-#include "client.h"
-#include "../common/udp.h"
+#include "../common/defs.h"
+#include "../common/connection.h"
 #include "../common/message.h"
-#include "../common/chase.h"
+#include "../common/chase_frontend.h"
 
+int dgram_socket;
+game game_state;
+screen game_screen = {NULL, NULL};
+player_position_t player;
 
-int main(int argc, char **argv ){;
-    char client_addr[108];
-    struct sockaddr_un serv_addr;
+// Parse the messages
+// ----
 
-    init_window(my_win, message_win);
-    cbreak();				/* Line buffering disabled	*/
-
-    sprintf(client_addr, "/tmp/client%d", getpid());
-    dgram_socket = unix_socket_init(client_addr);
-    serv_addr = getAddr(argv[1]);
-
-    client_loop(serv_addr);
-    
-    unlink(client_addr);
-    exit(0);
+void parse_ball_info(message *msg) {
+    sscanf(msg->txt, "%*s %d %d %c", &player.x, &player.y, &player.c);
+    player.health = MAX_HP;
+    game_state = msg->state;
 }
 
-//create client_loop that opens a udp socket and sends a connect message to the server and then implements a while that allows the user to move the player and sends the new position to the server unless the user presses q to quit or esc
-void client_loop(struct sockaddr_un serv_addr){
-    char msg[100];
-
-    player_position_t *my_player;
-    my_player = init_client(my_win);
-    
-    sprintf(msg, "connect");
-    send_msg(msg, serv_addr);
-    receive(my_player);
-
-    int key=-1;
-    while(key != 'q' && key != 27){
-        key = wgetch(my_win);
-
-        if (key == KEY_LEFT || key == KEY_RIGHT || key == KEY_UP || key == KEY_DOWN){
-            wprintw(my_win, "key: %d\n", key);
-            sprintf(msg, "move %d", key);
-            send_msg(msg, serv_addr);
-            receive(my_player);
-        }
-        if(my_player->health == 0){
-            break;
-        }
-    }
-    send_msg("disconnect", serv_addr);
-    free(my_player);
+void parse_field_status(message *msg) {
+    sscanf(msg->txt, "%*s %d %d %d", &player.x, &player.y, &player.health);
+    game_state = msg->state;
 }
 
-void send_msg(char *msg_txt, struct sockaddr_un servaddr){
+void parse_dead(message *msg) {
+    player.health = 0;
+    game_state = msg->state;
+}
+// ----
+
+void send_msg(const char *msg_txt, struct sockaddr_un servaddr){
     char command[16];
     message msg;
     strncpy(msg.txt, msg_txt, MSG_TXT_SIZE);
@@ -75,32 +51,72 @@ void send_msg(char *msg_txt, struct sockaddr_un servaddr){
     }else if(strcmp(command, "disconnect") == 0){
         sendto(dgram_socket, &msg, sizeof(msg), 0, (struct sockaddr *) &servaddr, sizeof(servaddr));
     }
-
 }
 
-void receive(player_position_t *my_player){
-    message buffer;
+void receive(){ // Receive a message, parse it and update state
+    message msg;
     char command[16];
 
-    if(recv(dgram_socket, &buffer, sizeof(buffer), 0) == 0){
+    if(recv(dgram_socket, &msg, sizeof(msg), 0) == 0){
         perror("Error receiving message, message is empty");
         exit(-1);
     }
 
-    sscanf(buffer.txt, "%s", command);
+    sscanf(msg.txt, "%s", command);
     
     if(strcmp(command, "ball_info") == 0){
-        sscanf(buffer.txt, "%*s %d %d %c", &my_player->x, &my_player->y, &my_player->c);
-        my_player->health = MAX_HP;
-        render_message(buffer, my_win, message_win);        
-
+        parse_ball_info(&msg);
     }else if(strcmp(command, "field_status") == 0){
-        sscanf(buffer.txt, "%*s %d %d %d", &my_player->x, &my_player->y, &my_player->health);
-        render_message(buffer, my_win, message_win);
-
+        parse_field_status(&msg);
     }else if(strcmp(command, "dead") == 0){
-        my_player->health = 0;
-        render_message(buffer, my_win, message_win);
+        parse_dead(&msg);
+    }
+}
+
+//create client_loop that opens a udp socket and sends a connect message to the server and then implements a while that allows the user to move the player and sends the new position to the server unless the user presses q to quit or esc
+void client_loop(struct sockaddr_un serv_addr){
+    char msg[100];
+    
+    send_msg("connect", serv_addr);
+    receive();
+
+    int key=-1;
+    while(key != 'q' && key != 27){
+        key = wgetch(game_screen.game_window);
+
+        if (key == KEY_LEFT || key == KEY_RIGHT || key == KEY_UP || key == KEY_DOWN){
+            wprintw(game_screen.game_window, "key: %d\n", key);
+            sprintf(msg, "move %d", key);
+            send_msg(msg, serv_addr);
+            receive();
+        }
+
+        render(game_screen, &game_state);
+
+        if(player.health == 0){
+            break;
+        }
     }
 
+    send_msg("disconnect", serv_addr);
+}
+
+int main(int argc, char **argv ){;
+    if (argc < 2) {
+        printf("Please specify the server address\n");
+        return 0;
+    }
+
+    char client_addr[108];
+
+    init_window(&game_screen);
+    cbreak();				/* Line buffering disabled	*/
+
+    sprintf(client_addr, "/tmp/client%d", getpid());
+    dgram_socket = unix_socket_init(client_addr);
+
+    client_loop(get_addr(argv[1]));
+    
+    unlink(client_addr);
+    exit(0);
 }
